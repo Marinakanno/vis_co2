6cff8fb7b094b9a4d180181802f3a2f006bd67d5
Marinakanno <74957627+Marinakanno@users.noreply.github.com>
2022-06-06 21:08:00 +0900

    add 変更

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="VISCO2", # 設定する名前
-    version="0.0.5", # バージョン設定
+    name="visco2", # 設定する名前
+    version="0.0.7", # バージョン設定
     author="marina kanno", # 名前
     author_email="marinasvtmax@gmail.com", # メアド変更
     description='Visualization of co2 emissions from 1990 to 2018 for each country', # 説明文書書き換え
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/plumchloride/kotonohatango-getter", # GitHubURL
+    url="https://github.com/Marinakanno/vis_co2", # GitHubURL
     project_urls={
-        "Bug Tracker": "https://github.com/plumchloride/kotonohatango-getter", #GitHubURL
+        "Bug Tracker": "https://github.com/Marinakanno/vis_co2", #GitHubURL
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,12 +21,12 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    py_modules=['VISCO2'], # 設定するモジュール名
+    py_modules=['visco2'], # 設定するモジュール名
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
     entry_points = {
         'console_scripts': [
-            'VISCO2 = VISCO2:main' # srcの中にある.pyの手前の文字
+            'visco2 = visco2:main' # srcの中にある.pyの手前の文字
         ]
     },
 )
